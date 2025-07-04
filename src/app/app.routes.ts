e50a44a5b3bd29e10e7f041cd5f9ac3b36763cc8
WilliamRG2004 <willrg2323@gmail.com>
2025-07-04 17:17:46 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,101 +1,278 @@
 import { Routes } from '@angular/router';
 import { TerapiaComponent } from './components/terapia/terapia.component';
-import { InsertareditarComponent } from './components/terapia/insertareditar/insertareditar.component';
-import { MusicacategoriaComponent } from './components/musicacategoria/musicacategoria.component';
-import { InsertareditarmucategoriaComponent } from './components/musicacategoria/insertareditarmucategoria/insertareditarmucategoria.component';
+import { InsertareditarComponentTerapia } from './components/terapia/insertareditar/insertareditar.component';
 import { PlaylistComponent } from './components/playlist/playlist.component';
-import { InsertareditarplaylistComponent } from './components/playlist/insertareditarplaylist/insertareditarplaylist.component';
-import { TipomaterialComponent } from './components/tipomaterial/tipomaterial.component';
-import { InsertareditartipomaterialComponent } from './components/tipomaterial/insertareditartipomaterial/insertareditartipomaterial.component';
-import { SesionterapiaComponent } from './components/sesionterapia/sesionterapia.component';
-import { InsertareditarsesionterapiaComponent } from './components/sesionterapia/insertareditarsesionterapia/insertareditarsesionterapia.component';
-import { ListarsesionporusuarioComponent } from './components/sesionterapia/listarsesionporusuario/listarsesionporusuario.component';
-import { ListarsesioncompletadoporusuarioComponent } from './components/sesionterapia/listarsesioncompletadoporusuario/listarsesioncompletadoporusuario.component';
-import { ListarusuariomoresesionComponent } from './components/sesionterapia/listarusuariomoresesion/listarusuariomoresesion.component';
-import { ListarterapiamoresesionComponent } from './components/sesionterapia/listarterapiamoresesion/listarterapiamoresesion.component';
-import { RoleComponent } from './components/role/role.component';
-import { InsertareditarroleComponent } from './components/role/insertareditarrole/insertareditarrole.component';
-import { UsersComponent } from './components/users/users.component';
-import { InsertareditarusersComponent } from './components/users/insertareditarusers/insertareditarusers.component';
-import { ProgresomusicarelajacionComponent } from './components/progresomusicarelajacion/progresomusicarelajacion.component';
-import { InsertareditarprogresomusicarelajacionComponent } from './components/progresomusicarelajacion/insertareditarprogresomusicarelajacion/insertareditarprogresomusicarelajacion.component';
-import { ListarmusicacompletaComponent } from './components/progresomusicarelajacion/listarmusicacompleta/listarmusicacompleta.component';
-import { PorcentajeprogresoComponent } from './components/progresomusicarelajacion/porcentajeprogreso/porcentajeprogreso.component';
+import { InsertareditarComponentPlaylist } from './components/playlist/insertareditar/insertareditar.component';
+import { CategoriaMusicalComponent } from './components/categoria-musical/categoria-musical.component';
+import { InsertareditarComponentCategoriaMusical } from './components/categoria-musical/insertareditar/insertareditar.component';
+import { TipoMaterialComponent } from './components/tipo-material/tipo-material.component';
+import { InsertareditarComponentTipoMaterial } from './components/tipo-material/insertareditar/insertareditar.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { TecnicaMeditacionComponent } from './components/tecnica-meditacion/tecnica-meditacion.component';
+import { InsertareditartecnicameditacionComponentTecnicaMeditacion } from './components/tecnica-meditacion/insertareditartecnicameditacion/insertareditartecnicameditacion.component';
+import { ChatbotComponent } from './components/chatbot/chatbot.component';
+import { GaleriaRelajanteComponent } from './components/galeria-relajante/galeria-relajante.component';
+import { VideoTecnicaRespiracionComponent } from './components/video-tecnica-respiracion/video-tecnica-respiracion.component';
+import { InsertareditarComponentVideoTecnicaRespiracion } from './components/video-tecnica-respiracion/insertareditar/insertareditar.component';
+import { InsertareditarComponentUsuario } from './components/usuario/insertareditar/insertareditar.component';
+import { RolComponent } from './components/rol/rol.component';
+import { InsertareditarComponentRol } from './components/rol/insertareditar/insertareditar.component';
+import { PagoComponent } from './components/pago/pago.component';
+import { InsertareditarComponentPago } from './components/pago/insertareditar/insertareditar.component';
+import { BilleteraElectronicaComponent } from './components/billetera-electronica/billetera-electronica.component';
+import { InsertareditarComponentBilleteraElectronica } from './components/billetera-electronica/insertareditar/insertareditar.component';
+import { MaterialMeditacionComponent } from './components/material-meditacion/material-meditacion.component';
+import { InsertareditarComponentMaterialMeditacion } from './components/material-meditacion/insertareditar/insertareditar.component';
+import { MusicaRelajacionComponent } from './components/musica-relajacion/musica-relajacion.component';
+import { InsertareditarComponentMusicaRelajacion } from './components/musica-relajacion/insertareditar/insertareditar.component';
+import { SesionTerapiaComponent } from './components/sesion-terapia/sesion-terapia.component';
+import { InsertareditarComponentSesionTerapia } from './components/sesion-terapia/insertareditar/insertareditar.component';
+import { MembresiaComponent } from './components/membresia/membresia.component';
+import { InsertareditarComponentMembresia } from './components/membresia/insertareditar/insertareditar.component';
+import { ProgresoMaterialMeditacionComponent } from './components/progreso-material-meditacion/progreso-material-meditacion.component';
+import { InsertareditarComponentProgresoMaterialMeditacion } from './components/progreso-material-meditacion/insertareditar/insertareditar.component';
+import { ProgresoMusicaComponent } from './components/progreso-musica/progreso-musica.component';
+import { InsertareditarComponentProgresoMusicaRelajacion } from './components/progreso-musica/insertareditar/insertareditar.component';
+import { ProgresoVideoComponent } from './components/progreso-video/progreso-video.component';
+import { InsertareditarComponentProgresoVideo } from './components/progreso-video/insertareditar/insertareditar.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+import { InsertareditarComponentTarjeta } from './components/tarjeta/insertareditar/insertareditar.component';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { LoginComponent } from './components/login/login.component';
+import { seguridadGuard } from './guard/seguridad.guard';
 
 export const routes: Routes = [
-  {
-    path:'',redirectTo:'terapia',pathMatch:'full'
-  },
-  {
-    path: 'terapia',
-    component: TerapiaComponent,
-    children: [
-      { path: 'nuevoterapia', component: InsertareditarComponent },
-      { path: 'edicionesterapia/:id', component: InsertareditarComponent }
-    ]
-  },
-  {
-    path: 'musicacategoria',
-    component: MusicacategoriaComponent,
-    children: [
-      { path: 'nuevomusicacategoria', component: InsertareditarmucategoriaComponent },
-      { path: 'edicionesmusicacategoria/:id', component: InsertareditarmucategoriaComponent }
-    ]
-  },
-  {
-    path: 'playlist',
-    component: PlaylistComponent,
-    children: [
-      { path: 'nuevoplaylist', component: InsertareditarplaylistComponent },
-      { path: 'edicionesplaylist/:id', component: InsertareditarplaylistComponent }
-    ]
-  },
-  {
-    path: 'tipomaterial',
-    component: TipomaterialComponent,
-    children: [
-      { path: 'nuevotipomaterial', component: InsertareditartipomaterialComponent },
-      { path: 'edicionestipomaterial/:id', component: InsertareditartipomaterialComponent }
-    ]
-  },
-  {
-    path: 'sesionterapia',
-    component: SesionterapiaComponent,
-    children: [
-      { path: 'nuevosesionterapia', component: InsertareditarsesionterapiaComponent },
-      { path: 'edicionessesionterapia/:id', component: InsertareditarsesionterapiaComponent },
-      { path: 'listporusuariosesionterapia', component: ListarsesionporusuarioComponent },
-      { path: 'listporusuariocompletosesionterapia', component: ListarsesioncompletadoporusuarioComponent },
-      { path: 'listporusuariomoresesionsesionterapia', component: ListarusuariomoresesionComponent },
-      { path: 'listporterapiamoresesionsesionterapia', component: ListarterapiamoresesionComponent }
-
-    ]
-  },
-  {
-    path: 'role',
-    component: RoleComponent,
-    children: [
-      { path: 'nuevorole', component: InsertareditarroleComponent },
-      { path: 'edicionesrole/:id', component: InsertareditarroleComponent }
-    ]
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      { path: 'nuevousers', component: InsertareditarusersComponent },
-      { path: 'edicionesusers/:id', component: InsertareditarusersComponent }
-    ]
-  },
-  {
-    path: 'progresomusicarelax',
-    component: ProgresomusicarelajacionComponent,
-    children: [
-      { path: 'nuevoprogresomusicarelax', component: InsertareditarprogresomusicarelajacionComponent },
-      { path: 'edicionesprogresomusicarelax/:id', component: InsertareditarprogresomusicarelajacionComponent },
-      { path: 'musicacompletada', component: ListarmusicacompletaComponent },
-      { path: 'porcentajeprogreso', component: PorcentajeprogresoComponent }
-    ]
-  }
-  
+    {
+        path:'',redirectTo:'menuCalmSphere',pathMatch:'full'
+    },
+    {
+        path: 'login', component:LoginComponent,
+    },
+    {
+        path:'terapias', component:TerapiaComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentTerapia
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentTerapia
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path:'playlists', component:PlaylistComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentPlaylist
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentPlaylist
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path:'categoriasmusicales', component:CategoriaMusicalComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentCategoriaMusical
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentCategoriaMusical
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path:'tiposmateriales', component:TipoMaterialComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentTipoMaterial
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentTipoMaterial
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'usuarios/nuevo', component:InsertareditarComponentUsuario
+    },
+    {
+        path:'usuarios', component:UsuarioComponent,
+        children:[
+            {
+                path:'ediciones/:id', component:InsertareditarComponentUsuario
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path:'tecnicasmeditaciones', component:TecnicaMeditacionComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditartecnicameditacionComponentTecnicaMeditacion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditartecnicameditacionComponentTecnicaMeditacion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'chatbot', component:ChatbotComponent,
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'galeriasrelajantes', component:GaleriaRelajanteComponent,
+        canActivate: [seguridadGuard],
+    },
+    {
+        path:'videostecnicasrespiraciones', component:VideoTecnicaRespiracionComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentVideoTecnicaRespiracion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentVideoTecnicaRespiracion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'roles', component:RolComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentRol
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentRol
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'pagos', component:PagoComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentPago
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentPago
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'billeteraselectronicas', component:BilleteraElectronicaComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentBilleteraElectronica
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentBilleteraElectronica
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'materialesmeditaciones', component:MaterialMeditacionComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentMaterialMeditacion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentMaterialMeditacion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'musicasrelajaciones', component:MusicaRelajacionComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentMusicaRelajacion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentMusicaRelajacion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'sesionesterapias', component:SesionTerapiaComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentSesionTerapia
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentSesionTerapia
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'membresias', component:MembresiaComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentMembresia
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentMembresia
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'progresosmaterialesmeitaciones', component:ProgresoMaterialMeditacionComponent,
+        children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentProgresoMaterialMeditacion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentProgresoMaterialMeditacion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'progresosmusicasrelajaciones', component:ProgresoMusicaComponent,
+         children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentProgresoMusicaRelajacion
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentProgresoMusicaRelajacion
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'progresosvideos', component:ProgresoVideoComponent,
+         children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentProgresoVideo
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentProgresoVideo
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'tarjetas', component:TarjetaComponent,
+         children:[
+            {
+                path: 'nuevo', component:InsertareditarComponentTarjeta
+            },
+            {
+                path:'ediciones/:id', component:InsertareditarComponentTarjeta
+            }
+        ],
+        canActivate: [seguridadGuard],
+    },
+    {
+        path: 'menuCalmSphere', component:MenuPrincipalComponent,
+    },
 ];
