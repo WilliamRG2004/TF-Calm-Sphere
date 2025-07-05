52a05a8253eadbc4dc9f7c3dd6e08430324762d2
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-07-05 15:53:05 -0500

    Update listarsesionterapia.component.ts

--- src/app/components/sesionterapia/listarsesionterapia/listarsesionterapia.component.ts
+++ src/app/components/sesionterapia/listarsesionterapia/listarsesionterapia.component.ts
@@ -22,5 +22,45 @@ import { SesionTerapiaService } from '../../../services/sesion-terapia.service';
   styleUrl: './listarsesionterapia.component.css'
 })
 export class ListarsesionterapiaComponent implements OnInit{
+  displayedColumns:string[]=['c1','c2','c3','c4','c5','c6','c7','c8'];
+  dataSource:MatTableDataSource<SesionTerapia>=new MatTableDataSource()
+  constructor(private seteraserv:SesionTerapiaService){}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   
+  dialog = inject(MatDialog)
+
+  ngOnInit(): void {
+      this.seteraserv.list().subscribe(data=>{
+        this.dataSource=new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      })
+
+      this.seteraserv.getList().subscribe(data=>{
+        this.dataSource=new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      })
+  }
+
+  openDialog(id:number){
+    const dialogRef = this.dialog.open(ConfirmComponent);
+    dialogRef.afterClosed().subscribe(result =>{
+      if(result){
+        this.eliminar(id);
+      }else{
+        console.log("Diálogo respondió no eliminar");
+      }
+    });
+  }
+
+  eliminar(id:number){
+    this.seteraserv.deleteST(id).subscribe(data=>{
+      this.seteraserv.list().subscribe(data=>{
+        this.seteraserv.setList(data)
+      })
+    })
+  }
 }
