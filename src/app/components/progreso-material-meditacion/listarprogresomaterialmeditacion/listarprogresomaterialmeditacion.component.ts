d5fb437878b9a8d132e81af4f232b64dfc4129ce
Enmanuelx98 <aloisio.forever140@gmail.com>
2025-07-06 18:49:13 -0500

    Update listarprogresomaterialmeditacion.component.ts

--- src/app/components/progreso-material-meditacion/listarprogresomaterialmeditacion/listarprogresomaterialmeditacion.component.ts
+++ src/app/components/progreso-material-meditacion/listarprogresomaterialmeditacion/listarprogresomaterialmeditacion.component.ts
@@ -69,13 +69,4 @@ export class ListarprogresomaterialmeditacionComponent implements OnInit{
     });
   }
 
-  private setDataSourceAttributes(): void {
-    if (this.paginator) {
-      this.dataSource.paginator = this.paginator;
-    }
-  }
 
-  esJoven(): boolean {
-    return this.role === 'JOVENESPROFESIONALES';
-  }
-}
